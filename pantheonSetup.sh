24248fcdebfb6881604d839a67aef97fc64d9289
AanthonyRusso <157141848+AanthonyRusso@users.noreply.github.com>
2025-04-15 14:53:31 -0500

    pantheonSetup.sh now correctly creates a trace file

--- pantheonSetup.sh
+++ pantheonSetup.sh
@@ -4,4 +4,7 @@ git submodule update --init --recursive
 echo "installing dependencies"
 tools/install_deps.sh
 src/experiments/setup.py --install-deps --all
-echo "12" > 1mbps.trace
+echo "creating needed trace file"
+echo "12" > src/experiments/1mbps.trace
+echo "Done!"
+echo "Run schemesTesting.sh to test cubic, vegas, and bbr"
