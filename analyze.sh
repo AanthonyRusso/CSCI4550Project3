275296bb42d672c12c02f853d7241f1e04bb9bd3
AanthonyRusso <157141848+AanthonyRusso@users.noreply.github.com>
2025-04-15 17:19:54 -0500

    updated  shell files for testing the schemes

--- analyze.sh
+++ analyze.sh
@@ -1,6 +1,2 @@
-src/analysis/analyze.py --data-dir bbrResults
-src/analysis/analyze.py --data-dir bbrResultsBad
-src/analysis/analyze.py --data-dir cubicResults
-src/analysis/analyze.py --data-dir cubicResultsBad
-src/analysis/analyze.py --data-dir vegasResults
-src/analysis/analyze.py --data-dir vegasResultsBad
+src/analysis/analyze.py --data-dir fullResultsBad
+src/analysis/analyze.py --data-dir fullResults
--- schemesTesting.sh
+++ schemesTesting.sh
@@ -2,21 +2,10 @@ echo "Making sure schemes needed are set up"
 src/experiments/setup.py --setup --schemes "bbr"
 src/experiments/setup.py --setup --schemes "cubic"
 src/experiments/setup.py --setup --schemes "vegas"
-echo "Cubic test 12mbps 10ms RTT..."
-src/experiments/test.py local --schemes "cubic" --data-dir cubicResults --uplink-trace src/experiments/12mbps.trace --downlink-trace src/experiments/12mbps.trace --prepend-mm-cmds "mm-delay 5" --runtime 60
+sudo sysctl -w net.ipv4.ip_forward=1
+echo "Testing BBR, Cubic, Vegas with 200 ms RTT"
+src/experiments/test.py local --schemes "bbr cubic vegas" --data-dir fullResultsBad --uplink-trace src/experiments/1mbps.trace --downlink-trace src/experiments/1mbps.trace --prepend-mm-cmds "mm-delay 100" --runtime 60
 
-echo "Cubic test 1mbps 200 RTT running..."
-src/experiments/test.py local --schemes "cubic" --data-dir cubicResultsBad --uplink-trace src/experiments/1mbps.trace --downlink-trace src/experiments/1mbps.trace --prepend-mm-cmds "mm-delay 100" --runtime 60
-
-echo "Vegas test 12mbps 10ms RTT..."
-src/experiments/test.py local --schemes "vegas" --data-dir vegasResults --uplink-trace src/experiments/12mbps.trace --downlink-trace src/experiments/12mbps.trace --prepend-mm-cmds "mm-delay 5" --runtime 60
-
-echo "Vegas test 1mbps 200 RTT running..."
-src/experiments/test.py local --schemes "vegas" --data-dir vegasResultsBad --uplink-trace src/experiments/1mbps.trace --downlink-trace src/experiments/1mbps.trace --prepend-mm-cmds "mm-delay 100" --runtime 60
-
-echo "Copa test 12mbps 10ms RTT..."
-src/experiments/test.py local --schemes "bbr" --data-dir bbrResults --uplink-trace src/experiments/12mbps.trace --downlink-trace src/experiments/12mbps.trace --prepend-mm-cmds "mm-delay 5" --runtime 60
-
-echo "Copa test 1mbps 200 RTT running..."
-src/experiments/test.py local --schemes "bbr" --data-dir bbrResultsBad --uplink-trace src/experiments/1mbps.trace --downlink-trace src/experiments/1mbps.trace --prepend-mm-cmds "mm-delay 100" --runtime 60
+echo "Testin BBR, Cubic, Vegas with 5 ms RTT, 12mbps"
+src/experiments/test.py local --schemes "bbr cubic vegas" --data-dir fullResults --uplink-trace src/experiments/12mbps.trace --downlink-trace src/experiments/12mbps.trace --prepend-mm-cmds "mm-delay 5" --runtime 60
 
